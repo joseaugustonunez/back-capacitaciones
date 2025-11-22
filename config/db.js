5a45a760c9cc15f0c43dd1b9e153d3b524cbd4a9
joseaugustonunez <joseaugustonunezvicente@gmail.com>
2025-11-22 10:05:53 -0500

    db nueva

--- config/db.js
+++ config/db.js
@@ -2,9 +2,9 @@ const mysql = require('mysql2/promise');
 
 const db = mysql.createPool({
   host: 'localhost',
-  user: 'capacitaciones',
+  user: 'capacitaciones', //localhost
   password: 'G1CaDUHcoC8hwFZrrA00',
-  database: 'plataformacapacitaciones',
+  database: 'plataformacapacitaciones', //plataforma_capacitaciones
 });
 
 module.exports = db;
